d0a913987d771abe1a4c6849ee56aa2241bf2387
barou <barou2000@hotmail.com>
2016-03-01 00:22:05 +0100

    Gestion des exceptions Ajout de SocieteService

--- src/main/java/fr/dauphine/secondMarket/sm_webapp/service/SocieteService.java
+++ src/main/java/fr/dauphine/secondMarket/sm_webapp/service/SocieteService.java
@@ -6,16 +6,25 @@ package fr.dauphine.secondMarket.sm_webapp.service;
 import java.util.List;
 
 import fr.dauphine.secondMarket.sm_webapp.domain.Societe;
+import fr.dauphine.secondMarket.sm_webapp.exception.SmDaoException;
+import fr.dauphine.secondMarket.sm_webapp.exception.SmTechException;
 
 /**
  * @author gnepa.rene.barou
  *
  */
 public interface SocieteService {
-	public Societe create(Societe societe);
-	public Societe delete(Long id) ;
-	public List<Societe> findAll();
-	public Societe update(Societe societe) ;
-	public Societe findById(Long id);
+	
+	void create(Societe newSociete) throws SmDaoException, SmTechException;
+
+	void delete(int id) throws SmDaoException;
+
+	List<Societe> findAll() throws SmDaoException;
+
+	void update(Societe societeToUpdate) throws SmDaoException;
+
+	Societe findById(int id) throws SmDaoException;
+
+	void accredit(int id) throws SmDaoException;
 
 }
